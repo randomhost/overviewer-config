1fc3c160f78d337416a483044c853020f7c9b231
Ch'Ih-Yu <chi-yu@web.de>
2020-07-12 13:21:53 +0200

    Enable icons in legend

--- config/overviewer/markers.py
+++ config/overviewer/markers.py
@@ -18,48 +18,56 @@ markers = [
         name='Builder Kommentare',
         filterFunction=lecternFilter,
         icon='icons/marker_comment.png',
+        showIconInLegend=True,
         checked=False
     ),
     dict(
         name='Spieler',
         filterFunction=playerFilter,
         icon='icons/marker_player.png',
+        showIconInLegend=True,
         checked=False
     ),
     dict(
         name='Parks & Öffentliche Plätze',
         filterFunction=filterBuilder('Park', 'Parks & Öffentliche Plätze'),
         icon='icons/marker_park.png',
+        showIconInLegend=True,
         checked=True
     ),
     dict(
         name='Kultur & Bildung',
         filterFunction=filterBuilder('Culture', 'Kultur & Bildung'),
         icon='icons/marker_culture.png',
+        showIconInLegend=True,
         checked=True
     ),
     dict(
         name='Handel & Industrie',
         filterFunction=filterBuilder('Commerce', 'Handel & Industrie'),
         icon='icons/marker_commerce.png',
+        showIconInLegend=True,
         checked=True
     ),
     dict(
         name='Privatgebäude',
         filterFunction=filterBuilder('Private', 'Privatgebäude'),
         icon='icons/marker_private.png',
+        showIconInLegend=True,
         checked=True
     ),
     dict(
         name='Militär',
         filterFunction=filterBuilder('Military', 'Militär'),
         icon='icons/marker_military.png',
+        showIconInLegend=True,
         checked=True
     ),
     dict(
         name='U-Bahnhof',
         filterFunction=filterBuilder('Subway', 'U-Bahnhof'),
         icon='icons/marker_subway.png',
+        showIconInLegend=True,
         checked=True
     )
 ]
